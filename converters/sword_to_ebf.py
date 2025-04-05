93d37d5e22a51df5ca4ff8571294424973cb523a
Natanael de Souza Figueiredo <natanael.souza.figueiredo@gmail.com>
2025-04-04 22:20:50 -0300

    Identify USFM id by lookup table

--- converters/sword_to_ebf.py
+++ converters/sword_to_ebf.py
@@ -4,6 +4,400 @@ from pysword.modules import SwordModules
 import argparse
 import json
 
+
+USFM_ID_LOOKUP_TABLE = [
+    {
+        "name": "Genesis",
+        "abbreviation": "Gen",
+        "usfm_id": "GEN"
+    },
+    {
+        "name": "Exodus",
+        "abbreviation": "Exod",
+        "usfm_id": "EXO"
+    },
+    {
+        "name": "Leviticus",
+        "abbreviation": "Lev",
+        "usfm_id": "LEV"
+    },
+    {
+        "name": "Numbers",
+        "abbreviation": "Num",
+        "usfm_id": "NUM"
+    },
+    {
+        "name": "Deuteronomy",
+        "abbreviation": "Deut",
+        "usfm_id": "DEU"
+    },
+    {
+        "name": "Joshua",
+        "abbreviation": "Josh",
+        "usfm_id": "JOS"
+    },
+    {
+        "name": "Judges",
+        "abbreviation": "Judg",
+        "usfm_id": "JDG"
+    },
+    {
+        "name": "Ruth",
+        "abbreviation": "Ruth",
+        "usfm_id": "RUT"
+    },
+    {
+        "name": "I Samuel",
+        "abbreviation": "1Sam",
+        "usfm_id": "1SA"
+    },
+    {
+        "name": "II Samuel",
+        "abbreviation": "2Sam",
+        "usfm_id": "2SA"
+    },
+    {
+        "name": "I Kings",
+        "abbreviation": "1Kgs",
+        "usfm_id": "1KI"
+    },
+    {
+        "name": "II Kings",
+        "abbreviation": "2Kgs",
+        "usfm_id": "2KI"
+    },
+    {
+        "name": "I Chronicles",
+        "abbreviation": "1Chr",
+        "usfm_id": "1CH"
+    },
+    {
+        "name": "II Chronicles",
+        "abbreviation": "2Chr",
+        "usfm_id": "2CH"
+    },
+    {
+        "name": "Ezra",
+        "abbreviation": "Ezra",
+        "usfm_id": "EZR"
+    },
+    {
+        "name": "Nehemiah",
+        "abbreviation": "Neh",
+        "usfm_id": "NEH"
+    },
+    {
+        "name": "Tobit",
+        "abbreviation": "Tob",
+        "usfm_id": "TOB"
+    },
+    {
+        "name": "Judith",
+        "abbreviation": "Jdt",
+        "usfm_id": "JDT"
+    },
+    {
+        "name": "Esther",
+        "abbreviation": "Esth",
+        "usfm_id": "EST"
+    },
+    {
+        "name": "Job",
+        "abbreviation": "Job",
+        "usfm_id": "JOB"
+    },
+    {
+        "name": "Psalms",
+        "abbreviation": "Ps",
+        "usfm_id": "PSA"
+    },
+    {
+        "name": "Proverbs",
+        "abbreviation": "Prov",
+        "usfm_id": "PRO"
+    },
+    {
+        "name": "Ecclesiastes",
+        "abbreviation": "Eccl",
+        "usfm_id": "ECC"
+    },
+    {
+        "name": "Song of Solomon",
+        "abbreviation": "Song",
+        "usfm_id": "SNG"
+    },
+    {
+        "name": "Wisdom",
+        "abbreviation": "Wis",
+        "usfm_id": "WIS"
+    },
+    {
+        "name": "Sirach",
+        "abbreviation": "Sir",
+        "usfm_id": "SIR"
+    },
+    {
+        "name": "Isaiah",
+        "abbreviation": "Isa",
+        "usfm_id": "ISA"
+    },
+    {
+        "name": "Jeremiah",
+        "abbreviation": "Jer",
+        "usfm_id": "JER"
+    },
+    {
+        "name": "Lamentations",
+        "abbreviation": "Lam",
+        "usfm_id": "LAM"
+    },
+    {
+        "name": "Baruch",
+        "abbreviation": "Bar",
+        "usfm_id": "BAR"
+    },
+    {
+        "name": "Ezekiel",
+        "abbreviation": "Ezek",
+        "usfm_id": "EZK"
+    },
+    {
+        "name": "Daniel",
+        "abbreviation": "Dan",
+        "usfm_id": "DAN"
+    },
+    {
+        "name": "Hosea",
+        "abbreviation": "Hos",
+        "usfm_id": "HOS"
+    },
+    {
+        "name": "Joel",
+        "abbreviation": "Joel",
+        "usfm_id": "JOL"
+    },
+    {
+        "name": "Amos",
+        "abbreviation": "Amos",
+        "usfm_id": "AMO"
+    },
+    {
+        "name": "Obadiah",
+        "abbreviation": "Obad",
+        "usfm_id": "OBA"
+    },
+    {
+        "name": "Jonah",
+        "abbreviation": "Jonah",
+        "usfm_id": "JON"
+    },
+    {
+        "name": "Micah",
+        "abbreviation": "Mic",
+        "usfm_id": "MIC"
+    },
+    {
+        "name": "Nahum",
+        "abbreviation": "Nah",
+        "usfm_id": "NAM"
+    },
+    {
+        "name": "Habakkuk",
+        "abbreviation": "Hab",
+        "usfm_id": "HAB"
+    },
+    {
+        "name": "Zephaniah",
+        "abbreviation": "Zeph",
+        "usfm_id": "ZEP"
+    },
+    {
+        "name": "Haggai",
+        "abbreviation": "Hag",
+        "usfm_id": "HAG"
+    },
+    {
+        "name": "Zechariah",
+        "abbreviation": "Zech",
+        "usfm_id": "ZEC"
+    },
+    {
+        "name": "Malachi",
+        "abbreviation": "Mal",
+        "usfm_id": "MAL"
+    },
+    {
+        "name": "I Maccabees",
+        "abbreviation": "1Macc",
+        "usfm_id": "1MA"
+    },
+    {
+        "name": "II Maccabees",
+        "abbreviation": "2Macc",
+        "usfm_id": "2MA"
+    },
+    {
+        "name": "Matthew",
+        "abbreviation": "Matt",
+        "usfm_id": "MAT"
+    },
+    {
+        "name": "Mark",
+        "abbreviation": "Mark",
+        "usfm_id": "MRK"
+    },
+    {
+        "name": "Luke",
+        "abbreviation": "Luke",
+        "usfm_id": "LUK"
+    },
+    {
+        "name": "John",
+        "abbreviation": "John",
+        "usfm_id": "JHN"
+    },
+    {
+        "name": "Acts",
+        "abbreviation": "Acts",
+        "usfm_id": "ACT"
+    },
+    {
+        "name": "Romans",
+        "abbreviation": "Rom",
+        "usfm_id": "ROM"
+    },
+    {
+        "name": "I Corinthians",
+        "abbreviation": "1Cor",
+        "usfm_id": "1CO"
+    },
+    {
+        "name": "II Corinthians",
+        "abbreviation": "2Cor",
+        "usfm_id": "2CO"
+    },
+    {
+        "name": "Galatians",
+        "abbreviation": "Gal",
+        "usfm_id": "GAL"
+    },
+    {
+        "name": "Ephesians",
+        "abbreviation": "Eph",
+        "usfm_id": "EPH"
+    },
+    {
+        "name": "Philippians",
+        "abbreviation": "Phil",
+        "usfm_id": "PHP"
+    },
+    {
+        "name": "Colossians",
+        "abbreviation": "Col",
+        "usfm_id": "COL"
+    },
+    {
+        "name": "I Thessalonians",
+        "abbreviation": "1Thess",
+        "usfm_id": "1TH"
+    },
+    {
+        "name": "II Thessalonians",
+        "abbreviation": "2Thess",
+        "usfm_id": "2TH"
+    },
+    {
+        "name": "I Timothy",
+        "abbreviation": "1Tim",
+        "usfm_id": "1TI"
+    },
+    {
+        "name": "II Timothy",
+        "abbreviation": "2Tim",
+        "usfm_id": "2TI"
+    },
+    {
+        "name": "Titus",
+        "abbreviation": "Titus",
+        "usfm_id": "TIT"
+    },
+    {
+        "name": "Philemon",
+        "abbreviation": "Phlm",
+        "usfm_id": "PHM"
+    },
+    {
+        "name": "Hebrews",
+        "abbreviation": "Heb",
+        "usfm_id": "HEB"
+    },
+    {
+        "name": "James",
+        "abbreviation": "Jas",
+        "usfm_id": "JAS"
+    },
+    {
+        "name": "I Peter",
+        "abbreviation": "1Pet",
+        "usfm_id": "1PE"
+    },
+    {
+        "name": "II Peter",
+        "abbreviation": "2Pet",
+        "usfm_id": "2PE"
+    },
+    {
+        "name": "I John",
+        "abbreviation": "1John",
+        "usfm_id": "1JN"
+    },
+    {
+        "name": "II John",
+        "abbreviation": "2John",
+        "usfm_id": "2JN"
+    },
+    {
+        "name": "III John",
+        "abbreviation": "3John",
+        "usfm_id": "3JN"
+    },
+    {
+        "name": "Jude",
+        "abbreviation": "Jude",
+        "usfm_id": "JUD"
+    },
+    {
+        "name": "Revelation of John",
+        "abbreviation": "Rev",
+        "usfm_id": "REV"
+    },
+    {
+        "name": "Prayer of Manasses",
+        "abbreviation": "PrMan",
+        "usfm_id": "MAN"
+    },
+    {
+        "name": "I Esdras",
+        "abbreviation": "1Esd",
+        "usfm_id": "1ES"
+    },
+    {
+        "name": "II Esdras",
+        "abbreviation": "2Esd",
+        "usfm_id": "2ES"
+    },
+    {
+        "name": "Additional Psalm",
+        "abbreviation": "AddPs",
+        "usfm_id": "PS2"
+    },
+    {
+        "name": "Laodiceans",
+        "abbreviation": "EpLao",
+        "usfm_id": "LAO"
+    }
+]
+
 def generate_dict(source_file):
     modules = SwordModules(source_file)
     found_modules = modules.parse_modules()
@@ -36,10 +430,15 @@ def generate_dict(source_file):
                 verses.append({"text": verse_text,})
             chapters.append({"verses": verses})
         if not all_verses_empty:
-            # TODO: get actual USFM id of the book
+            # Find the USFM ID based on abbreviation
+            usfm_id = next(
+                (item['usfm_id'] for item in USFM_ID_LOOKUP_TABLE 
+                if item['abbreviation'] == book.preferred_abbreviation),
+                None
+            )
             books_out_list.append({
                 'names': [book.name],
-                'usfm_id': book.name,
+                'usfm_id': usfm_id,
                 'abbreviation': book.preferred_abbreviation,
                 'chapters': chapters
             })
